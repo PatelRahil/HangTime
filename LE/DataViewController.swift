781dc883ec2754cf99c562406ae52dd6d5a9683b
Rahil Patel <rahilpatel99@gmail.com>
2017-05-14 23:11:23 -0400

    Fixed SlideView bug and double transition bug

--- LE/DataViewController.swift
+++ LE/DataViewController.swift
@@ -18,18 +18,40 @@ class DataViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var passwordTextBox: UITextField!
     @IBOutlet weak var usernameTextBox: UITextField!
 
-    
+   /*
     @IBAction func LoginButton(_ sender: Any, forEvent event: UIEvent) {
+        print("HEY?")
         FIRAuth.auth()?.signIn(withEmail: usernameTextBox.text!, password: passwordTextBox.text!) { (user, error) in
+            print("w_orked")
             if error == nil {
                 self.performSegue(withIdentifier: "LoginSegue", sender: sender)
+                print("worked")
             }
             else {
                 print(error!)
             }
         }
         
+    } 
+    */
+    @IBAction func LoginButton(_ sender: Any) {
+        FIRAuth.auth()?.signIn(withEmail: usernameTextBox.text!, password: passwordTextBox.text!) { (user, error) in
+            if error == nil {
+                self.performSegue(withIdentifier: "LoginSegue", sender: sender)
+            }
+            else {
+                if (String(describing: error!) == "Error Domain=FIRAuthErrorDomain Code=17009 \"The password is invalid or the user does not have a password.\" UserInfo={NSLocalizedDescription=The password is invalid or the user does not have a password., error_name=ERROR_WRONG_PASSWORD}") {
+                    
+                    print("******************\nInvalid Password\n*******************")
+                }
+                if (String(describing: error!) == "Error Domain=FIRAuthErrorDomain Code=17008 \"The email address is badly formatted.\" UserInfo={NSLocalizedDescription=The email address is badly formatted., error_name=ERROR_INVALID_EMAIL}") {
+                    
+                    print("******************\nInvalid Email\n*******************")
+                }
+            }
+        }
     }
+
     @IBAction func newAccountButton(_ sender: UIButton, forEvent event: UIEvent) {
         try! FIRAuth.auth()!.signOut()
         self.performSegue(withIdentifier: "NewAccountSegue", sender: sender)
