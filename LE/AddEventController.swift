bdfbb33e90750b7ab08681395de41753bc8a0e2d
Rahil Patel <rahilpatel99@gmail.com>
2017-05-27 22:27:23 -0400

    Limited description textfield to 40 char

--- LE/AddEventController.swift
+++ LE/AddEventController.swift
@@ -12,6 +12,36 @@ import CoreLocation
 import FirebaseDatabase
 import Firebase
 
+//For limiting the length of a textfield
+private var __maxLengths = [UITextField: Int]()
+extension UITextField {
+    @IBInspectable var maxLength: Int {
+        get {
+            guard let l = __maxLengths[self] else {
+                return 150 // (global default-limit. or just, Int.max)
+            }
+            return l
+        }
+        set {
+            __maxLengths[self] = newValue
+            addTarget(self, action: #selector(fix), for: .editingChanged)
+        }
+    }
+    func fix(textField: UITextField) {
+        let t = textField.text
+        textField.text = t?.safelyLimitedTo(length: maxLength)
+    }
+}
+
+extension String
+{
+    func safelyLimitedTo(length n: Int)->String {
+        let c = self.characters
+        if (c.count <= n) { return self }
+        return String( Array(c).prefix(upTo: n) )
+    }
+}
+
 class AddEventController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var DescriptionTextbox: UITextField!
     @IBOutlet weak var AddressTextbox:
@@ -222,3 +252,5 @@ class AddEventController: UIViewController, UITextFieldDelegate {
         //self.dataLabel!.text = dataObject
     }
 }
+
+
